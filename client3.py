b3c22f2aa0cff1ee9df1292988a4e4eb66ff863c
Evenson Charles <evensoncharles@Evensons-MacBook-Pro.local>
2023-12-30 19:22:33 -0500

    task 1 complete

--- client3.py
+++ client3.py
@@ -35,14 +35,16 @@ def getDataPoint(quote):
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    price = (bid_price + ask_price) / 2
     return stock, bid_price, ask_price, price
 
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
-    return 1
+    # ratio is easy just make sure price_b is not 0 which would give an error
+    if (price_b == 0): return 
+    return price_a/price_b
 
 
 # Main
@@ -52,8 +54,11 @@ if __name__ == "__main__":
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
         """ ----------- Update to get the ratio --------------- """
+        # had to see what stock prices the ratio was using 
+        # stored the price of the tickers in a hash/array with key = stock and value = price
+        stk_prices = {}
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
+            stk_prices[stock] = price
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
-
-        print("Ratio %s" % getRatio(price, price))
+        print("Ratio %s" % getRatio(stk_prices["ABC"], stk_prices["DEF"]))
